49e0d6aa3fb2b677ca56c6163d5010b41b3a863c
Seth Geyer <seth.geyer@gmail.com>
2013-06-24 15:30:56 -0600

    Round 3 on inactive status

--- app/controllers/main_controller.rb
+++ app/controllers/main_controller.rb
@@ -11,6 +11,9 @@ before_filter do
   if !OPEN_PAGES.include?(request.path_info) && session["found_florist_id"] == nil && session["found_user_id"] == nil
     render(:login, layout:false) and return
   end
+  if Employee.where(id: session["found_user_id"]).first.status == "Inactive" || Florist.where(id: session["found_florist_id"]).first.status == "Inactive"
+    render(:login, layout:false) and return
+  end
 end
 
 
