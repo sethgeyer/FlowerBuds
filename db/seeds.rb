afc2641eda9b6b8db0c0163afdf899269f747d76
Seth Geyer <seth.geyer@gmail.com>
2013-06-27 09:02:50 -0600

    revised seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,22 +7,19 @@
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 
 
+=
 florists = Florist.create([
-    {name: "Fundingsland Flowers", company_logo: "bouquet.jpeg", company_id: "funland"},
-    {name: "April's Flowers", company_logo: "aprils_flowers.jpeg", company_id: "april"},
-    {name: "Perfect Petal", company_logo: "perfect_petal.jpeg", company_id: "perfect"}, 
-    {name: "Wild Orchid", company_logo: "wild_orchid.jpeg", company_id: "orchid"},
-    {name: "Bloom", company_logo: "bloom.jpeg", company_id: "bloom"}
+    {name: "flowerbuds", company_logo: "logo_flowerbuds.jpeg", company_id: "flowerbuds", created_at: "2013-06-18 17:26:49", updated_at: "2013-06-20 15:45:41", updated_by: "admin", status: "Active", city: "", state: "", zip: ""}
+    
     ])
     
     
     
 employees = Employee.create([
-    {name: "Jenny Geyer", status: "Active", username: "jenny", password: "jenny", florist_id: 1, admin_rights: ""},
-    {name: "Debbie Downer", status: "Active", username: "debbie", password: "debbie", florist_id: 1, admin_rights: ""},
-    {name: "Lisa Smith", status: "Active", username: "lisa", password: "lisa", florist_id: 2, admin_rights: ""},
+    {name: "admin", status: "Active", email: "", w_phone: "", c_phone: "", employee_type: "", username: "admin", password_digest: "$2a$10$fy6TazpmelyibYDAEoiSkOaiYQFC2.GxO.6YNYpdbPyB...", florist_id: 6, admin_rights: "All Admin Rights", password: nil, created_at: "2013-06-19 02:11:21", updated_at: "2013-06-23 16:34:07", updated_by: nil, primary_poc: "yes", view_pref: "all"}
     ])
     
+=begin
 customers = Customer.create([
   {name: "Sara Bolle", florist_id: 1},
   {name: "Emily Furgeson", florist_id: 1},
@@ -30,7 +27,9 @@ customers = Customer.create([
   {name: "Sally Smith", florist_id: 2},
   {name: "Heather Leslie", florist_id: 2}
   ])
-  
+=end
+
+=begin  
 products = Product.create([
 {product_type: "Labor", name: "Dandelions", items_per_bunch: 25.0, cost_per_bunch: 45.0, cost_for_one: 1.8, markup: 3.0, status: "Active", florist_id: 1},
 {product_type: "freshies", name: nil, items_per_bunch: 30.0, cost_per_bunch: 30.0, cost_for_one: 1.0, markup: 3.0, status: "Active", florist_id: 1},
@@ -65,3 +64,4 @@ products = Product.create([
 {product_type: "Fresh", name: "Anemone", items_per_bunch: 10.0, cost_per_bunch: 14.95, cost_for_one: 1.495, markup: 3.0, status: "Active", florist_id: 2},
 {product_type: "Fresh", name: "Green Mist", items_per_bunch: 10.0, cost_per_bunch: 8.95, cost_for_one: 0.895, markup: 3.0, status: "Active", florist_id: 2}
 ])
+=end
