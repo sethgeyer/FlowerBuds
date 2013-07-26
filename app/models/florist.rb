158d3d1381ad483b2ade1a75f7c8ce49e3bbca19
Seth Geyer <seth.geyer@gmail.com>
2013-07-25 18:03:34 -0600

    redirected routing and eliminated old-style image. Incorporated in DropZone

--- app/models/florist.rb
+++ app/models/florist.rb
@@ -8,7 +8,7 @@ class Florist < ActiveRecord::Base
   has_many :quotes
   has_many :specifications
   has_many :images 
-  attr_accessible :name, :company_logo, :company_id
+  attr_accessible :name, :company_id
   validates_presence_of :name, :company_id, :status
   validates_uniqueness_of :company_id
 end
